daf35370a1c22949bdb01c51732262e4c7ef814b
Denis Chornyi <chornyi.den@gmail.com>
2024-01-01 15:39:54 +0200

    Gromcode__JS-full.git-class-4(3)

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ for (let x = 1; x <= 5; x++) {
   for (let y = 1; y <= 10; y++) {
     console.log(x + ' x ' + y + ' = ' + x * y);
   }
-  console.log('=> ' + x + ' <=');
+  console.log('\n=> ' + x + ' <=\n\n');
 }
