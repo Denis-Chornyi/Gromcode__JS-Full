96c256c0b34ae2b62c67f68631eafa6fbee20f6f
Denis Chornyi <chornyi.den@gmail.com>
2024-01-09 15:56:00 +0200

    Gromcode__JS-full-arrays-methods

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-for (let i = 1; i <= 5; i += 1) {
-  console.log("\n=> " + i + " <=\n\n");
-  for (let m = 1; m <= 10; m += 1) {
-    console.log(i + " x " + m + " = " + i * m);
-  }
-}
+const flatArray = (arr) => {
+  const copyArr = arr.slice();
+  const flatArr = copyArr.flat();
+  flatArr.sort((a, b) => (a > b ? 1 : -1));
+  return flatArr;
+};
