dc88dc3cf768b339273c7cf5d05c0d888a41eb20
Denis Chornyi <chornyi.den@gmail.com>
2024-02-06 13:25:54 +0200

    JS-full_events(1)

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
-export const getTitle = () => {
-  const titleContent = document.querySelector(".title").textContent;
-  return titleContent;
-};
-getTitle();
+const elemDiv = document.querySelector(".rect_div");
+const elemP = document.querySelector(".rect_p");
+const elemSpan = document.querySelector(".rect_span");
+const eventsListElem = document.querySelector(".events-list");
+const clearButton = document.querySelector(".clear-btn");
+const removeHandlers = document.querySelector(".remove-handlers-btn");
+const attachHandlers = document.querySelector(".attach-handlers-btn");
 
-export const getDescription = () => {
-  const aboutContent = document.querySelector(".about").innerText;
-  return aboutContent;
-};
-getDescription();
+eventsListElem.innerHTML = "";
 
-export const getPlans = () => {
-  const plansContent = document.querySelector(".plans").innerHTML;
-  return plansContent;
+const clearBtn = () => {
+  eventsListElem.innerHTML = "";
 };
-getPlans();
-
-
-export const getGoal = () => {
-  const goalContent = document.querySelector(".goal").outerHTML;
-  return goalContent;
+const logTarget = (text, color) => {
+  eventsListElem.innerHTML += `<span style="color: ${color}; margin-left: 8px;">${text}</span>`;
 };
-getGoal();
+const logGreenDiv = logTarget.bind(null, "DIV", "Green");
+const logGreenP = logTarget.bind(null, "P", "Green");
+const logGreenSpan = logTarget.bind(null, "SPAN", "Green");
+
+const logGreyDiv = logTarget.bind(null, "DIV", "Grey");
+const logGreyP = logTarget.bind(null, "P", "Grey");
+const logGreySpan = logTarget.bind(null, "SPAN", "Grey");
+
+elemDiv.addEventListener("click", logGreyDiv, true);
+elemP.addEventListener("click", logGreyP, true);
+elemSpan.addEventListener("click", logGreySpan, true);
+
+elemDiv.addEventListener("click", logGreenDiv);
+elemP.addEventListener("click", logGreenP);
+elemSpan.addEventListener("click", logGreenSpan);
+
+clearButton.addEventListener("click", clearBtn);
+
+const stopProcess = () => {
+  elemDiv.removeEventListener("click", logGreenDiv);
+  elemP.removeEventListener("click", logGreenP);
+  elemSpan.removeEventListener("click", logGreenSpan);
+  elemDiv.removeEventListener("click", logGreyDiv, true);
+  elemP.removeEventListener("click", logGreyP, true);
+  elemSpan.removeEventListener("click", logGreySpan, true);
+};
+
+removeHandlers.addEventListener("click", stopProcess);
+const startProcess = () => {
+  removeHandlers.removeEventListener("click", stopProcess);
+};
+attachHandlers.addEventListener("click", startProcess);
+
