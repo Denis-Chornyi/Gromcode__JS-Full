8d76aeca7d2639e4a8fa4f00fec480fa2be1061a
Denis Chornyi <chornyi.den@gmail.com>
2024-02-06 16:35:26 +0200

    JS-full_events(1)

--- index.js
+++ index.js
@@ -9,20 +9,22 @@ const logEvent = (text, color) => {
   spanElem.textContent = text;
   eventsListElem.appendChild(spanElem);
 };
-const logGreenDiv = logEvent.bind(null, "div", "Green");
-const logGreenP = logEvent.bind(null, "p", "Green");
-const logGreenSpan = logEvent.bind(null, "span", "Green");
 
-const logGreyDiv = logEvent.bind(null, "div", "Grey");
-const logGreyP = logEvent.bind(null, "p", "Grey");
-const logGreySpan = logEvent.bind(null, "span", "Grey");
+const logGreenDiv = logEvent.bind(null, "div", "green");
+const logGreyDiv = logEvent.bind(null, "div", "grey");
+
+const logGreenP = logEvent.bind(null, "p", "green");
+const logGreyP = logEvent.bind(null, "p", "grey");
+
+const logGreenSpan = logEvent.bind(null, "span", "green");
+const logGreySpan = logEvent.bind(null, "span", "grey");
 
 const attachHandlers = () => {
   divElem.addEventListener("click", logGreyDiv, true);
-  pElem.addEventListener("click", logGreyP, true);
-  spanElem.addEventListener("click", logGreySpan, true);
   divElem.addEventListener("click", logGreenDiv);
+  pElem.addEventListener("click", logGreyP, true);
   pElem.addEventListener("click", logGreenP);
+  spanElem.addEventListener("click", logGreySpan, true);
   spanElem.addEventListener("click", logGreenSpan);
 };
 
@@ -31,77 +33,23 @@ attachHandlersBtnElem.addEventListener("click", attachHandlers);
 
 const removeHandlers = () => {
   divElem.removeEventListener("click", logGreenDiv);
-  pElem.removeEventListener("click", logGreenP);
-  spanElem.removeEventListener("click", logGreenSpan);
   divElem.removeEventListener("click", logGreyDiv, true);
+  pElem.removeEventListener("click", logGreenP);
   pElem.removeEventListener("click", logGreyP, true);
+  spanElem.removeEventListener("click", logGreenSpan);
   spanElem.removeEventListener("click", logGreySpan, true);
 };
 
 const removeHandlersBtnElem = document.querySelector(".remove-handlers-btn");
 removeHandlersBtnElem.addEventListener("click", removeHandlers);
 
-const clearBtn = () => {
+const clearBoard = () => {
   eventsListElem.innerHTML = "";
 };
-const clearButton = document.querySelector(".clear-btn");
-clearButton.addEventListener("click", clearBtn);
+
+const clearBtnElem = document.querySelector(".clear-btn");
+clearBtnElem.addEventListener("click", clearBoard);
 
 document.addEventListener("DOMContentLoaded", () => {
   attachHandlers();
 });
-// const divElem = document.querySelector("div");
-// const pElem = document.querySelector("p");
-// const spanElem = document.querySelector("span");
-// const eventsListElem = document.querySelector(".events-list");
-
-// const logEvent = (text, color) => {
-//   const spanElem = document.createElement("span");
-//   spanElem.style = `color: ${color}; margin-left: 8px;`;
-//   spanElem.textContent = text;
-//   eventsListElem.appendChild(spanElem);
-// };
-
-// const logGreenDiv = logEvent.bind(null, "div", "green");
-// const logGreyDiv = logEvent.bind(null, "div", "grey");
-
-// const logGreenP = logEvent.bind(null, "p", "green");
-// const logGreyP = logEvent.bind(null, "p", "grey");
-
-// const logGreenSpan = logEvent.bind(null, "span", "green");
-// const logGreySpan = logEvent.bind(null, "span", "grey");
-
-// const attachHandlers = () => {
-//   divElem.addEventListener("click", logGreyDiv, true);
-//   divElem.addEventListener("click", logGreenDiv);
-//   pElem.addEventListener("click", logGreyP, true);
-//   pElem.addEventListener("click", logGreenP);
-//   spanElem.addEventListener("click", logGreySpan, true);
-//   spanElem.addEventListener("click", logGreenSpan);
-// };
-
-// const attachHandlersBtnElem = document.querySelector(".attach-handlers-btn");
-// attachHandlersBtnElem.addEventListener("click", attachHandlers);
-
-// const removeHandlers = () => {
-//   divElem.removeEventListener("click", logGreenDiv);
-//   divElem.removeEventListener("click", logGreyDiv, true);
-//   pElem.removeEventListener("click", logGreenP);
-//   pElem.removeEventListener("click", logGreyP, true);
-//   spanElem.removeEventListener("click", logGreenSpan);
-//   spanElem.removeEventListener("click", logGreySpan, true);
-// };
-
-// const removeHandlersBtnElem = document.querySelector(".remove-handlers-btn");
-// removeHandlersBtnElem.addEventListener("click", removeHandlers);
-
-// const clearBoard = () => {
-//   eventsListElem.innerHTML = "";
-// };
-
-// const clearBtnElem = document.querySelector(".clear-btn");
-// clearBtnElem.addEventListener("click", clearBoard);
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   attachHandlers();
-// });
