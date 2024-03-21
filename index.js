b3b693b836e9103f545c786b48e980df4918f47e
Denis Chornyi <chornyi.den@gmail.com>
2024-03-21 16:17:50 +0200

    js-full

--- index.js
+++ index.js
@@ -16,29 +16,25 @@
 // console.log(findSolution(104))
 
 // ///////////////////////////////////
+const baseUrl = "https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks";
 
-const userAvatarElem = document.querySelector(".user__avatar");
-const userNameElem = document.querySelector(".user__name");
-const userLocationElem = document.querySelector(".user__location");
+function getTasksList() {
+  // put your code here
+  return fetch(baseUrl).then((response) => response.json());
+}
 
-const fetchUserData = (userName) =>
-  fetch(`https://api.github.com/users/${userName}`).then((response) =>
-    response.json()
-  );
+function getTaskById(taskId) {
+  // put your code here
+  return fetch(baseUrl)
+    .then((response) => response.json())
+    .then((taskData) => taskData.filter((obj) => obj.id === taskId));
+}
 
-const renderUserData = (userData) => {
-  const { avatar_url, name, location } = userData;
-  userAvatarElem.src = avatar_url;
-  userNameElem.textContent = name;
-  userLocationElem.textContent = location ? `from ${location}` : "";
-};
+// examples
+getTasksList().then((tasksList) => {
+  console.log(tasksList); // ==> [ {'id':'1', 'isDone':false ... }, {'id':'2', 'isDone':false ... }, ...]
+});
 
-const showUserBtnElem = document.querySelector(".name-form__btn");
-const userNameInputElem = document.querySelector(".name-form__input");
-
-const onSearchUser = () => {
-  const userName = userNameInputElem.value;
-  fetchUserData(userName).then((userData) => renderUserData(userData));
-};
-
-showUserBtnElem.addEventListener("click", onSearchUser);
+getTaskById("2").then((taskData) => {
+  console.log(taskData); // ==> { 'id': '2', 'text': 'District Communications Specialist', 'isDone': false, 'createdDate': 1651499052, 'finishedDate': 1651499052 }
+});
